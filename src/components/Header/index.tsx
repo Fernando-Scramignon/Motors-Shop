dc944da56446622cf20d0ec2166e0c877eedbae3
Fernando Scramignon <scramignonnarde@gmail.com>
2022-12-19 19:42:15 -0300

    feat/Inicia a criação do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,69 @@
-function Header() {}
+import { StyledHeader, LogoContainer, MobileMenu, DesktopMenu } from "./style";
+
+import colorizedLogo from "../../assets/motorsShopColor.svg";
+import bars from "../../assets/bars.svg";
+
+import MobileHeaderModal from "../MobileHeaderModal";
+
+import { useState, useEffect } from "react";
+
+function Header() {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
+    const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+    function handleResize() {
+        if (window.innerWidth > 1024) return setIsDesktop(true);
+        return setIsDesktop(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+    });
+
+    return (
+        <>
+            <StyledHeader id="header">
+                <LogoContainer>
+                    <img
+                        className="colorizedLogo"
+                        src={colorizedLogo}
+                        alt="Motors Shop logo colorized"
+                    />
+                </LogoContainer>
+                {!isDesktop ? (
+                    <MobileMenu>
+                        <img className="bars" src={bars} alt="bars menu" />
+                    </MobileMenu>
+                ) : (
+                    <DesktopMenu isAuthenticated={isAuthenticated}>
+                        <div className="desktopMenu__options">
+                            <span>Carros</span>
+                            <span>Motos</span>
+                            <span>Leilão</span>
+                        </div>
+                        <div className="desktopMenu__separator"></div>
+                        {isAuthenticated ? (
+                            <div className="desktopMenu__profile">
+                                <div className="desktopMenu__profile--icon">
+                                    FS
+                                </div>
+                                <div className="desktopMenu__profile--name">
+                                    Fernando Scramignon
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="desktopMenu__singUp">
+                                <span>Fazer Login</span>
+                                {/* This is a placeholder */}
+                                <button>Cadastrar</button>
+                            </div>
+                        )}
+                    </DesktopMenu>
+                )}
+            </StyledHeader>
+            <MobileHeaderModal />
+        </>
+    );
+}
 
 export default Header;
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,1 +1,122 @@
 import styled from "styled-components";
+
+export interface IDesktopMenuProps {
+    isAuthenticated: boolean;
+}
+
+export const StyledHeader = styled.header`
+    height: 80px;
+    width: 100vw;
+    min-width: 320px;
+
+    padding: 0px 4.3%;
+    border-bottom: 2px solid var(--grey-6);
+
+    background: var(--grey-10);
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const LogoContainer = styled.section``;
+
+export const MobileMenu = styled.section``;
+
+export const DesktopMenu = styled.section<IDesktopMenuProps>`
+    display: flex;
+    gap: 44px;
+
+    span {
+        cursor: pointer;
+
+        transition: 0.3s;
+        &:hover {
+            filter: brightness(0.8);
+        }
+    }
+
+    .desktopMenu__options {
+        height: 80px;
+
+        display: flex;
+        align-items: center;
+        gap: 44px;
+
+        color: var(--grey-2);
+
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
+        font-size: 16px;
+    }
+
+    .desktopMenu__separator {
+        width: 2px;
+        height: 80px;
+        background: var(--grey-6);
+    }
+
+    .desktopMenu__singUp {
+        display: flex;
+        gap: 44px;
+        align-items: center;
+
+        color: var(--grey-2);
+
+        font-size: 16px;
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
+
+        button {
+            border: 1px solid black;
+            padding: 7px 20px;
+
+            background: none;
+            color: var(--grey-0);
+
+            font-size: 16px;
+            font-family: "Inter", sans-serif;
+            font-weight: 600;
+        }
+    }
+
+    .desktopMenu__profile {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+
+        cursor: pointer;
+
+        .desktopMenu__profile--icon {
+            width: 32px;
+            height: 32px;
+
+            background: var(--brand-2);
+            color: #ffffff;
+
+            border-radius: 150px;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            font-family: "Inter", sans-serif;
+            font-size: 14px;
+            font-weight: 700;
+        }
+
+        .desktopMenu__profile--name {
+            color: var(--grey-2);
+
+            font-family: "Inter", sans-serif;
+            font-weight: 400;
+            font-size: 16px;
+
+            cursor: pointer;
+            transition: 0.3s;
+            &:hover {
+                filter: brightness(0.5);
+            }
+        }
+    }
+`;
