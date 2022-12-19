c35b34eb6876ce2be17e596481c5968768e0fae1
Fernando Scramignon <scramignonnarde@gmail.com>
2022-12-19 10:47:00 -0300

    feat/Cria Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,20 @@
-function Footer() {}
+import { StyledFooter, GoUpButton, FooterParagraph } from "./style";
+
+import logo from "../../assets/motorsShop.svg";
+import angleUp from "../../assets/angleUp.svg";
+
+function Footer() {
+    return (
+        <StyledFooter>
+            <img src={logo} />
+            <FooterParagraph>
+                © 2022 - Todos os direitos reservados.
+            </FooterParagraph>
+            <GoUpButton>
+                <img src={angleUp} />
+            </GoUpButton>
+        </StyledFooter>
+    );
+}
 
 export default Footer;
