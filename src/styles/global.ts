bee09370042861057ee10939aa5e05b44b15a460
Fernando Scramignon <scramignonnarde@gmail.com>
2022-12-19 10:49:05 -0300

    fix/Adiciona box sizing

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,6 +20,7 @@ export const GlobalStyle = createGlobalStyle`
         font-size: 100%;
         font: inherit;
         vertical-align: baseline;
+        box-sizing: border-box;
     }
     article, aside, details, figcaption, figure,
     footer, header, hgroup, menu, nav, section {
@@ -87,4 +88,5 @@ export const GlobalStyle = createGlobalStyle`
         --random-11: #5700E3;
         --random-12: #30007D;
     }
+
 `;
