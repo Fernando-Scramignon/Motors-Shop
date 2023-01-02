52fe4aea4f9ab5020116158cbc14663499445cbf
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-01-02 15:44:14 -0300

    feat/Substitui mockup pela requisição da api

--- src/pages/ProfileViewUser/index.tsx
+++ src/pages/ProfileViewUser/index.tsx
@@ -10,61 +10,39 @@ import ProfileCard from "../../components/ProfileCard";
 import ProductCardList from "../../components/ProductCardList";
 
 import { IProductCard } from "../../components/ProductCardList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import carPhoto from "../../assets/carPhoto.png";
-import motorcyclePhoto from "../../assets/motorcyclePhoto.png";
-
-const car: IProductCard = {
-    id: "a",
-    cover_image: carPhoto,
-    description: "lorem ipsum",
-    km: 10000,
-    price: 150000,
-    published: true,
-    title: "title",
-    username: "Fernando Scramignon",
-    vehicle_type: "Carro",
-    year: 2021,
-};
-
-const moto: IProductCard = {
-    id: "a",
-    cover_image: motorcyclePhoto,
-    description: "vrummmmm",
-    km: 5000,
-    price: 90000,
-    published: true,
-    title: "title",
-    username: "Fernando Scramignon",
-    vehicle_type: "Moto",
-    year: 2078,
-};
-
-const carsList: IProductCard[] = [];
-const motoList: IProductCard[] = [];
-
-for (let i = 0; i < 6; i++) {
-    if (i % 2 !== 0) {
-        carsList.push({ ...car, id: String(i), published: false });
-        continue;
-    }
-    carsList.push({ ...car, id: String(i) });
-}
-
-for (let i = 0; i < 6; i++) {
-    if (i % 2 == 0) {
-        motoList.push({ ...moto, id: String(i * 10), published: false });
-        continue;
-    }
-    motoList.push({ ...moto, id: String(i * 10) });
-}
-
-for (let i = 0; i < 6; i++) {}
+import { APIRequests } from "../../services/api";
 
 function ProfileViewUser() {
-    const [cars, setCars] = useState(carsList);
-    const [motos, setMotos] = useState(motoList);
+    const [cars, setCars] = useState([]);
+    const [motos, setMotos] = useState([]);
+
+    useEffect(() => {
+        const mockedUserName = "Fernando Scramignon";
+
+        APIRequests.getProducts()
+            .then((response: any) => {
+                const vehiclesList = response.data.map(
+                    (vehicle: IProductCard) => {
+                        vehicle.username = mockedUserName;
+                        return vehicle;
+                    }
+                );
+
+                const carsList = vehiclesList.filter(
+                    (vehicle: IProductCard) => vehicle.vehicle_type === "Carro"
+                );
+
+                const bikeList = vehiclesList.filter(
+                    (vehicle: IProductCard) => vehicle.vehicle_type === "Moto"
+                );
+
+                setCars(carsList);
+                setMotos(bikeList);
+            })
+            .catch((response: any) => console.error(response.message));
+    }, []);
 
     return (
         <StyledProfileBody>
