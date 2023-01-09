f2074e1fe0c7cf7df73be3c2c4f2b8fca0522cce
Fernando-Scramignon <scramignonnarde@gmail.com>
2023-01-09 18:26:22 -0300

    feat/Cria auction card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,19 +4,13 @@ import UnderHeaderBackground from "../../components/UnderHeaderBackground";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ProductCardList from "../../components/ProductCardList";
+import ProductCardAuction from "../../components/ProductCardAuction";
 
 import { useState, useEffect, useContext } from "react";
 
 import { IFullProduct, ProductContext } from "../../providers/product";
 import { UserContext } from "../../providers/user";
 import { useNavigate } from "react-router-dom";
-
-// Criar interface e o objeto onde vão ficar as cordenadas y
-// Criar interface do header que aceita o objeto acima
-// Fazer ele ser opcional
-// Passar para o header como props
-// Adicinar função on click nos links de navegação
-//    Se a posição y correspondente existir, navege para ela, se não, faça nada
 
 function Home() {
     const [cars, setCars] = useState<IFullProduct[]>([]);
@@ -45,6 +39,7 @@ function Home() {
         <StyledHomeBody>
             <Header yPositions={{ carsY: 400, bikesY: 950 }} />
             <UnderHeaderBackground />
+            <ProductCardAuction />
             <ProductListSection>
                 <ProductCardList
                     advertise={false}
