e217506c8fe1aae29a01e54e061a0a3c1122083f
ErickMarchetti <erickgmarchetti@gmail.com>
2023-01-11 21:01:03 -0300

    feat/ modal de envio de token para recuperar senha

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,11 @@ import Button from "../../components/Button";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
-import { StyledLogin, StyledSuccessModal } from "./style";
+import {
+    StyledLogin,
+    StyledSuccessModal,
+    StyledForgotPasswordModal,
+} from "./style";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -11,12 +15,16 @@ import { useContext, useState } from "react";
 import { UserContext } from "../../providers/user";
 import FeedbackModal from "../../components/FeedbackModal";
 
-interface IFormInputs {
+interface IFormLoginInputs {
     email: string;
     password: string;
 }
 
-const schema = yup.object({
+interface IFormModalInputs {
+    emailInfo: string;
+}
+
+const loginSchema = yup.object({
     email: yup
         .string()
         .email("Coloque um email válido")
@@ -24,24 +32,39 @@ const schema = yup.object({
     password: yup.string().required("Campo obrigatório"),
 });
 
+const modalSchema = yup.object({
+    emailInfo: yup
+        .string()
+        .email("Coloque um email válido")
+        .required("Campo obrigatório"),
+});
+
 function Login() {
     const [success, setSuccess] = useState<boolean>(false);
+    const [forgotPasswordModal, setForgotPasswordModal] =
+        useState<boolean>(true);
+    const [emailSent, setEmailSent] = useState<boolean>(false);
     const navigate = useNavigate();
-    const { login, setIsAuthenticated } = useContext(UserContext);
+    const { login, setIsAuthenticated, forgotPassword } =
+        useContext(UserContext);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<IFormInputs>({
+    const loginForm = useForm<IFormLoginInputs>({
         mode: "onSubmit",
         reValidateMode: "onChange",
         shouldFocusError: false,
         shouldUnregister: false,
-        resolver: yupResolver(schema),
+        resolver: yupResolver(loginSchema),
     });
 
-    function onSubmit(data: IFormInputs) {
+    const modalForm = useForm<IFormModalInputs>({
+        mode: "onSubmit",
+        reValidateMode: "onChange",
+        shouldFocusError: false,
+        shouldUnregister: false,
+        resolver: yupResolver(modalSchema),
+    });
+
+    function onSubmit(data: IFormLoginInputs) {
         login(data).then((res) => {
             if (res) {
                 setSuccess(true);
@@ -50,28 +73,42 @@ function Login() {
         });
     }
 
+    function sendEmail(data: IFormModalInputs) {
+        forgotPassword(data.emailInfo).then((res) => {
+            if (res) {
+                setEmailSent(true);
+            }
+        });
+    }
+
     return (
         <StyledLogin>
             <Header />
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={loginForm.handleSubmit(onSubmit)}>
                 <h2>Login</h2>
                 <Input
-                    errors={errors}
+                    errors={loginForm.formState.errors}
                     label="Usuário"
                     name="email"
                     placeholder="Digitar email"
-                    register={register}
+                    register={loginForm.register}
                     type="text"
                 />
                 <Input
-                    errors={errors}
+                    errors={loginForm.formState.errors}
                     label="Senha"
                     name="password"
                     placeholder="Digitar senha"
-                    register={register}
+                    register={loginForm.register}
                     type="password"
                 />
-                <a>Esqueci minha senha</a>
+                <button
+                    className="form__button--forgotPassword"
+                    type="button"
+                    onClick={() => setForgotPasswordModal(true)}
+                >
+                    Esqueci minha senha
+                </button>
                 <Button
                     backgroundcolor="var(--brand-1)"
                     border="2px solid var(--brand-1)"
@@ -126,6 +163,53 @@ function Login() {
                         escala
                     </p>
                 </StyledSuccessModal>
+            </FeedbackModal>
+            <FeedbackModal
+                setState={setForgotPasswordModal}
+                state={forgotPasswordModal}
+                title="Esqueci minha senha"
+                onClose={() => emailSent && navigate("/")}
+            >
+                <StyledForgotPasswordModal
+                    as="form"
+                    onSubmit={modalForm.handleSubmit(sendEmail)}
+                >
+                    {!emailSent ? (
+                        <>
+                            <Input
+                                errors={modalForm.formState.errors}
+                                label="Informe seu email:"
+                                name="emailInfo"
+                                placeholder="Digitar email"
+                                register={modalForm.register}
+                                type="text"
+                            />
+
+                            <Button
+                                id="forgotPasswordModal__button--confirm"
+                                backgroundcolor="var(--brand-1)"
+                                border="0"
+                                color="var(--grey-10)"
+                                height="48px"
+                                width="100%"
+                                type="submit"
+                                size="big"
+                                hover={{
+                                    backgroundColorHover: "var(--brand-2)",
+                                    border: "0",
+                                    colorHover: "var(--white-fixed)",
+                                }}
+                            >
+                                Confirmar
+                            </Button>
+                        </>
+                    ) : (
+                        <span className="forgotPasswordModal__span--feedback">
+                            Foi enviado um token para o email informado, acesse
+                            a página de atualização de senha
+                        </span>
+                    )}
+                </StyledForgotPasswordModal>
             </FeedbackModal>
         </StyledLogin>
     );
