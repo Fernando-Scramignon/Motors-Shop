7871c9791d1060890307189a0b66e35496c5aa28
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-13 21:19:16 -0300

    build: changes api route for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import { IProductCard } from "../components/ProductCardList";
 
 import axios, { Axios } from "axios";
-
 const API_PORT = import.meta.env.VITE_API_PORT;
 
 const api: Axios = axios.create({
-    baseURL: `http://localhost:${API_PORT}`,
+    baseURL:
+        process.env.NODE_ENV == "production"
+            ? "https://motors-shop-api.onrender.com"
+            : `http://localhost:${API_PORT}`,
 });
 
 export class APIRequests {
